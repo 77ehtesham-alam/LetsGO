7f106f0e476841efbcf393b49a026220bbf42248
77ehtesham-alam <ehtesham.alam@apphelix.ai>
2025-05-09 15:39:52 +0530

    Update add_numbers.go

--- add_numbers.go
+++ add_numbers.go
@@ -10,6 +10,6 @@ func main() {
     fmt.Print("Enter second number: ")
     fmt.Scan(&num2)
     
-    sum := num1 + num2
+    sum := num1 + num
     fmt.Printf("The sum of %d and %d is %d\n", num1, num2, sum)
 }
