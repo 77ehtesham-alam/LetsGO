2590b6bce8dbacae338f4c5ab718117e12792422
77ehtesham-alam <ehtesham.alam@apphelix.ai>
2025-05-09 12:57:59 +0530

    Update add_numbers.go

--- add_numbers.go
+++ add_numbers.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
     var num1, num2 int
-    fmt.Print("Enter first number: ")
+    fmt.Print("Enter first numbers: ")
     fmt.Scan(&num1)
     
     fmt.Print("Enter second number: ")
